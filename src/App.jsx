245a54f2b70d5121981738138889a9fd34f05021
iren <dudareva.iryna.study@gmail.com>
2025-05-20 19:31:08 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import SearchBar from './components/SearchBar/SearchBar'
 import ImageGallery from './components/ImageGallery/ImageGallery'
 import Loader from './components/Loader/Loader'
 import ErrorMessage from './components/ErrorMessage/ErrorMessage'
-import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn'
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn' //change change change
 import ImageModal from './components/ImageModal/ImageModal'
 
 
